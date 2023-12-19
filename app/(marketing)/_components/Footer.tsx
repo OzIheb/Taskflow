4c57a6b3f53647f91631ae15de47f9ad5dfdec8e
oziheb <ouelhazi3iheb@gmail.com>
2023-12-19 20:24:47 +0100

    finish landing page

---
+++ app/(marketing)/_components/Footer.tsx
@@ -0,0 +1,25 @@
+import Logo from "@/components/logo"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+
+export const Footer = () => {
+    return (
+        <div className="fixed bottom-0 w-full px-4 border-t  bg-slate-100 ">
+            <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between p-2">
+                <Logo />
+                <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
+                    <Button size='sm' asChild variant="ghost">
+                        <Link href="/">
+                            Privacy Policy
+                        </Link>
+                    </Button>
+                    <Button size='sm' variant="ghost">
+                        <Link href="/" >
+                            Terms of Service
+                        </Link>
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ config/site.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+    name: "Taskflow",
+    description: "Taskflow is a task and project management tool for organisations.",
+}
